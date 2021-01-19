f21b859d9723eb11e3f0361df3bc5aa38b7c28e1
İsmet Taha <ismet.taha@gmail.com>
2021-01-20 02:06:15 +0300

    server fix

--- server.py
+++ server.py
@@ -28,6 +28,6 @@ def create_app():
     app.add_url_rule("/reports", view_func=views.reports)
     return app
 
+app = create_app()
 if __name__ == "__main__":
-    app = create_app()
     app.run()
